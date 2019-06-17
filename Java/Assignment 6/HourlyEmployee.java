21d94927aa894b4e501e621c5e8d7294d1f6b91f
gbutler32 <34986409+gbutler32@users.noreply.github.com>
2019-06-16 17:05:42 -0700

    Create HourlyEmployee.java

---
+++ Java/Assignment 6/HourlyEmployee.java
@@ -0,0 +1,86 @@
+/*CSCI 470    Assignment 6   Fall 2018
+ * 
+ * Developer: Gary Butler
+ * 
+ * Section: 1
+ * 
+ * Due Date/Time: 10/19/18 11:59pm
+ * 
+ * Purpose: This program creates objects that impliment the payable class and outputs them. If the class if of the
+ * type "BasePlusCommissionEmployee" the base salary is increased by 10%
+ */
+
+public class HourlyEmployee extends Employee implements Payable
+{
+   private double wage; // wage per hour
+   private double hours; // hours worked for week
+
+   // constructor
+   public HourlyEmployee(String firstName, String lastName,
+      String socialSecurityNumber, double wage, double hours)
+   {
+      super(firstName, lastName, socialSecurityNumber);
+
+      if (wage < 0.0) // validate wage
+         throw new IllegalArgumentException(
+            "Hourly wage must be >= 0.0");
+
+      if ((hours < 0.0) || (hours > 168.0)) // validate hours
+         throw new IllegalArgumentException(
+            "Hours worked must be >= 0.0 and <= 168.0");
+
+      this.wage = wage;
+      this.hours = hours;
+   } 
+
+   // set wage
+   public void setWage(double wage)
+   {
+      if (wage < 0.0) // validate wage
+         throw new IllegalArgumentException(
+            "Hourly wage must be >= 0.0");
+
+      this.wage = wage;
+   } 
+
+   // return wage
+   public double getWage()
+   {
+      return wage;
+   } 
+
+   // set hours worked
+   public void setHours(double hours)
+   {
+      if ((hours < 0.0) || (hours > 168.0)) // validate hours
+         throw new IllegalArgumentException(
+            "Hours worked must be >= 0.0 and <= 168.0");
+
+      this.hours = hours;
+   } 
+
+   // return hours worked
+   public double getHours()
+   {
+      return hours;
+   } 
+
+   // calculate earnings; override abstract method earnings in Employee
+   @Override                                                           
+   public double getPaymentAmount()                                            
+   {                                                                   
+      if (getHours() <= 40) // no overtime                           
+         return getWage() * getHours();                                
+      else                                                             
+         return 40 * getWage() + (getHours() - 40) * getWage() * 1.5;
+   }                                          
+
+   // return String representation of HourlyEmployee object              
+   @Override                                                             
+   public String toString()                                              
+   {                                                                     
+      return String.format("hourly employee: %s%n%s: $%,.2f; %s: %,.2f",
+         super.toString(), "hourly wage", getWage(),                     
+         "hours worked", getHours());                                   
+   }                                    
+} // end class HourlyEmployee
