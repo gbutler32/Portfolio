2854426c6db6dd53bbcf0e4f1d94467f701cab4f
gbutler32 <34986409+gbutler32@users.noreply.github.com>
2019-06-16 17:04:35 -0700

    Create BasePlusCommissionEmployee.java

---
+++ Java/Assignment 6/BasePlusCommissionEmployee.java
@@ -0,0 +1,61 @@
+/*CSCI 470    Assignment 6   Fall 2018
+ * 
+ * Developer: Gary Butler
+ * 
+ * Section: 1
+ * 
+ * Due Date/Time: 10/19/18 11:59pm
+ * 
+ * Purpose: This program creates objects that impliment the payable class and outputs them. If the class if of the
+ * type "BasePlusCommissionEmployee" the base salary is increased by 10%
+ */
+
+public class BasePlusCommissionEmployee extends CommissionEmployee  implements Payable
+{
+   private double baseSalary; // base salary per week
+
+   // constructor
+   public BasePlusCommissionEmployee(String firstName, String lastName, 
+      String socialSecurityNumber, double grossSales,
+      double commissionRate, double baseSalary)
+   {
+      super(firstName, lastName, socialSecurityNumber, 
+         grossSales, commissionRate);
+
+      if (baseSalary < 0.0) // validate baseSalary                  
+         throw new IllegalArgumentException("Base salary must be >= 0.0");
+            
+      this.baseSalary = baseSalary;                
+   }
+
+   // set base salary
+   public void setBaseSalary(double baseSalary)
+   {
+      if (baseSalary < 0.0) // validate baseSalary                  
+         throw new IllegalArgumentException("Base salary must be >= 0.0");
+            
+      this.baseSalary = baseSalary;                
+   } 
+
+   // return base salary
+   public double getBaseSalary()
+   {
+      return baseSalary;
+   }
+
+   // calculate earnings; override method earnings in CommissionEmployee
+   @Override                                                            
+   public double getPaymentAmount()                                             
+   {                                                                    
+      return getBaseSalary() + super.getPaymentAmount();                        
+   } 
+
+   // return String representation of BasePlusCommissionEmployee object
+   @Override                                                           
+   public String toString()                                            
+   {                                                                   
+      return String.format("%s %s; %s: $%,.2f",                       
+         "base-salaried", super.toString(),                            
+         "base salary", getBaseSalary());                             
+   } 
+} // end class BasePlusCommissionEmployee
